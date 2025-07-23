871f75165d99e301aa19428e202a1a910fc93cf6
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2025-07-24 00:45:34 +0900

    Initial Commit

---
+++ ZunTalk/ZunTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZunTalkApp.swift
+//  ZunTalk
+//
+//  Created by jumpei ono on 2025/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ZunTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZunTalkTests/ZunTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZunTalkTests.swift
+//  ZunTalkTests
+//
+//  Created by jumpei ono on 2025/07/24.
+//
+
+import Testing
+@testable import ZunTalk
+
+struct ZunTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
